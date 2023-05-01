e0e05ab0f47f3520d0d95eeab2771c205a5f654d
chris <chris@localhost.localdomain>
2023-05-01 11:23:11 +0100

    Challenge 2

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 console.log('Hello World');
 
-app.get("/",function(res,req){
+app.get("/",function(req,res){
     res.send("Hello Express");
   }
 )
