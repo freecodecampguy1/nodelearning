396a088931d31157057f18b6afdd4472189d9b93
chris <chris@localhost.localdomain>
2023-05-04 19:20:20 +0100

    Challenge 6

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
+
 console.log('Hello World');
 
 app.use("/public",express.static(__dirname+"/public"));
@@ -10,7 +12,13 @@ app.get("/",function(req,res){
 )
 
 app.get("/json",function(req,res){
-    res.json({message:"Hello json"})
+    if (process.env.MESSAGE_STYLE==uppercase){
+    res.json({message:"Hello json".toUpperCase()})
+    } else{
+      res.json({message:"Hello json"})
+
+    }
+
 });
 
 
