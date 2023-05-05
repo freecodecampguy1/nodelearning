bef99b5c8aeb6736cd12ca6d5a52831a9dbaf34d
chris <chris@localhost.localdomain>
2023-05-05 17:35:37 +0100

    Challenge 7

--- myApp.js
+++ myApp.js
@@ -6,10 +6,18 @@ console.log('Hello World');
 
 app.use("/public",express.static(__dirname+"/public"));
 
+app.use("/",function(req,res,next){
+        console.log(req.method+" "+req.path+" - "+req.ip)
+        next()
+});
+
+
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/views/index.html");
   }
 )
+
+
 
 app.get("/json",function(req,res){
     if (process.env.MESSAGE_STYLE=="uppercase"){
@@ -20,6 +28,7 @@ app.get("/json",function(req,res){
     }
 
 });
+
 
 
 
